86d4762bbe505395fbd2bf5e532ffaef3260e135
AtelB <Atelubright05@gmail.com>
2023-04-05 07:39:42 -0700

    Add solutions to task

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -13,6 +13,8 @@ listint_t *add_nodeint_end(listimt_t **head, const int n)
 		listint_t *new;
 		listint_t *temp;
 
+		if (head == NULL)
+			return (NULL);
 		new = malloc(sizeof(listint_t));
 		if (new == NULL)
 			return (NULL);
