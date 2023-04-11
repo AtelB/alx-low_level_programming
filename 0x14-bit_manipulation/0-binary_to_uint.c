eee18b31442293d262eb5306580466aab98b55d7
AtelB <Atelubright05@gmail.com>
2023-04-11 16:02:36 -0700

    Add solutions to task

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -2,16 +2,16 @@
 
 /**
  * binary_to_uint - converts a binary number to an usigned int
- * @b:pointer to a string carrying a binary number
+ * @b:pointer to a string of 0 and 1 chars
  *
- * Return: unsigned int or 0 if error is encountered
+ * Return: the converted number, or 0 if error
  */
 unsigned int binary_to_uint(const char *b);
 {
 	int i;
-	unsigned int num;
+	unsigned int n;
 
-	num = 0;
+	n = 0;
 	if (!b)
 		return (0);
 	for (i = 0; b[i] != '\0'; i++)
@@ -21,9 +21,9 @@ unsigned int binary_to_uint(const char *b);
 	}
 	for (i = 0; b[i] != '\0'; i++)
 	{
-		num <<= 1;
+		n <<= 1;
 		if (b[i] == '1')
-			num += 1;
+			n += 1;
 	}
-	return (num);
+	return (n);
 }
--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -1,7 +1,6 @@
-#ifndef _ALX_
-#define _ALX_
+#ifndef _MAIN_H_
+#define _MAIN_H_
 
-unsigned long int _power(unsigned int base, unsigned int pow);
 unsigned int binary_to_uint(const char *b);
 void print_binary(unsigned long int n);
 int _putchar(char c);
