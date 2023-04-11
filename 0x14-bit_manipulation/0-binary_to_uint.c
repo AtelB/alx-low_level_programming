e3930fd022ec29b98263b815ce2436fc17c9f0e0
AtelB <Atelubright05@gmail.com>
2023-04-11 16:29:41 -0700

    Add solutions to task

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -6,12 +6,12 @@
  *
  * Return: the converted number, or 0 if error
  */
-unsigned int binary_to_uint(const char *b);
+unsigned int binary_to_uint(const char *b)
 {
 	int i;
-	unsigned int n;
+	unsigned int num;
 
-	n = 0;
+	num = 0;
 	if (!b)
 		return (0);
 	for (i = 0; b[i] != '\0'; i++)
@@ -21,9 +21,9 @@ unsigned int binary_to_uint(const char *b);
 	}
 	for (i = 0; b[i] != '\0'; i++)
 	{
-		n <<= 1;
+		num <<= 1;
 		if (b[i] == '1')
-			n += 1;
+			num += 1;
 	}
-	return (n);
+	return (num);
 }
