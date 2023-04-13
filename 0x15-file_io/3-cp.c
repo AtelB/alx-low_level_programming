63bd512610bd92fecc633fe6329b547d5aa43e2c
AtelB <Atelubright05@gmail.com>
2023-04-13 08:39:30 -0700

    Add solutions to File IO

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,62 +1,70 @@
 #include "main.h"
 #include <stdio.h>
+#include <stdarg.h>
 
 /**
- * error_file - check to see if files can be opened.
- * @exit_code:the file_from.
- * @message:the file_to.
- * @argv: the arguments vector
- * Return: no return
+ * error_handler - handles the errors
+ * @exit_code: displays exit code
+ * @message: displays error message
+ * @type: data type
  */
-void error_file(int exit_code, char *message, char *argv[])
+
+void error_handler(int exit_code, char *message, char type, ...)
 {
-	if (exit_code == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-		exit(98);
-	}
-	if (*message == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-		exit(99);
-	}
+	va_list args;
+
+	va_start(args, type);
+	if (type == 's')
+		dprintf(STDERR_FILENO, message, va_arg(args, char *));
+	else if (type == 'd')
+		dprintf(STDERR_FILENO, message, va_arg(args, int));
+	else if (type == 'N')
+		dprintf(STDERR_FILENO, message, "");
+	else
+		dprintf(STDERR_FILENO, "Error: Does not match any type\n");
+	va_end(args);
+	exit(exit_code);
 }
 
 
 /**
- * main - copy one content of a file to another
- * @argc: the sum argument
- * @argv: arguments success
- *
- * Return: 1 on success, otherwise exit
+ * main - copies the content of a file to another file
+ * @argc: number of arguments
+ * @argv: arguments vector
+ * Return: Always (0).
  */
+
 int main(int argc, char *argv[])
 {
-	int fd_s, fd_d, n_read = 1024, wrote, close_fd_s, close_fd_d;
-	unsigned int mode = S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH;
-	char buffer[1024];
+	char buf[1024];
+	int fd_s, fd_d;
+
+	ssize_t nchars, nwr;
 
 	if (argc != 3)
-	{
-		dprintf(STDERR_FILENO, "%s", "Usage: cp file_from file_to\n");
-		exit(97);
-	}
+		error_handler(97, "Usage: cp file_from file_to\n", 'N');
+
 	fd_s = open(argv[1], O_RDONLY);
-	check_IO_stat(fd_s, -1, argv[1], 'O');
-	fd_d = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, mode);
-	check_IO_stat(fd_d, -1, argv[2], 'W');
-	while (n_read == 1024)
+	if (fd_s == -1)
+		error_handler(98, "Error: Can't read from file %s\n", 's', argv[1]);
+
+	fd_d = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+	if (fd_d == -1)
+		error_handler(99, "Error: Can't write to %s\n", 's', argv[2]);
+
+	while ((nchars = read(fd_s, buf, 1024)) > 0)
 	{
-		n_read = read(fd_s, buffer, sizeof(buffer));
-		if (n_read == -1)
-			check_IO_stat(-1, -1, argv[1], 'O');
-		wrote = write(fd_d, buffer, n_read);
-		if (wrote == -1)
-			check_IO_stat(-1, -1, argv[2], 'W');
+		nwr = write(fd_d, buf, nchars);
+		if (nwr == -1)
+			error_handler(99, "Error: Can't write to %s\n", 's', argv[2]);
 	}
-	close_fd_s = close(fd_s);
-	check_IO_stat(close_fd_s, fd_s, NULL, 'C');
-	close_fd_d = close(fd_d);
-	check_IO_stat(close_fd_d, fd_d, NULL, 'C');
+
+	if (nchars == -1)
+		error_handler(98, "Error: Can't read from file %s\n", 's', argv[1]);
+	if (close(fd_s) == -1)
+		error_handler(100, "Error: Can't close fd %d\n", 'd', fd_s);
+	if (close(fd_d) == -1)
+		error_handler(100, "Error: Can't close fd %d\n", 'd', fd_d);
+
 	return (0);
 }
